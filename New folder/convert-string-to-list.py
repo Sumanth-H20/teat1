4ae209d4cd2578f11dd4845ce40252256672185d
Sumanth-H20 <hsumanth.k@gmail.com>
2025-02-20 13:36:44 +0530

    first commit

--- New folder/convert-string-to-list.py
+++
@@ -1,9 +0,0 @@
-import os
-
-folders = input("please provide the proper paths separated by comma:").split()
-
-for folder in folders:
-    files =os.listdir(folder)
-        
-    for file in files:
-        print (file)
--- New folder/sample.py
+++
@@ -1,6 +0,0 @@
-f =open("welcome.txt", "r")
-line1=f.readline()
-print(line1)
-     
-line2=f.readline()
-print(line2)
